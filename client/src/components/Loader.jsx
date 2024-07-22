3b876886f13cd413401f19f79ed1b6c233932e63
praveenprk <praveenkumarkp666@gmail.com>
2024-07-23 01:25:08 +0530

    remove loader text instead just icon animating

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -4,8 +4,8 @@ const Loader = () => {
   return (
     <div className="flex items-center justify-center space-x-2">
       <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full" role="status">
-        <span className="visually-hidden">Loading...</span>
       </div>
+      {/* <span className="visually-hidden">Loading...</span> */}
     </div>
   );
 };
