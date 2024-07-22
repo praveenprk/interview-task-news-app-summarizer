b6ca07b4f67f3c49576356ac959b78b1ccfc5b1b
praveenprk <praveenkumarkp666@gmail.com>
2024-07-23 01:48:04 +0530

    show some text if the search bar is empty

--- client/src/components/NewsList.jsx
+++ client/src/components/NewsList.jsx
@@ -4,9 +4,9 @@ import NewsItem from './NewsItem';
 const NewsList = ({ articles }) => {
   return (
     <div className="space-y-4">
-      {articles.map((article, index) => (
+      {(articles.length > 0) ? articles.map((article, index) => (
         <NewsItem key={index} article={article} />
-      ))}
+      )) : <h4 className='text-center'>Search a trending keyword that comes to your mind...</h4>}
     </div>
   );
 };
