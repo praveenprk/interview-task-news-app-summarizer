d68ea30d8446fecb3061ff09eafffa564fc77596
praveenprk <praveenkumarkp666@gmail.com>
2024-07-23 01:48:48 +0530

    style news item component

--- client/src/components/NewsItem.jsx
+++ client/src/components/NewsItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { PublishedAt, Source, Summary } from '../utilities/Helper';
 
 const NewsItem = ({ article }) => {
   return (
-    <div>
-      <h2>{article.title}</h2>
-      <p>{article.source.name} - {new Date(article.publishedAt).toLocaleDateString()}</p>
-      <p>{article.summary}</p>
+    <div className="p-4 border border-gray-200 rounded-md shadow-md">
+      <h2 className="text-xl font-semibold">{article.title}</h2>
+      <p className="text-gray-500">
+       <Source />{article.source.name} - <PublishedAt/>{new Date(article.publishedAt).toLocaleDateString()}</p>
+      <p className="mt-2"><Summary />{article.summary}</p>
     </div>
   );
 };
