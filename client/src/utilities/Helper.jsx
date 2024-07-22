c2a4c2b28fa5ef689b9e2a2ab611774c26ca212e
praveenprk <praveenkumarkp666@gmail.com>
2024-07-23 01:24:50 +0530

    add helper components for showing static info

---
+++ client/src/utilities/Helper.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Source = () => {
+  return <span className='italic font-medium'>Source: {' '}</span>;
+}
+
+const Summary = () => {
+  return <span className='font-semibold'>Summary:{' '}</span>;
+}
+
+const PublishedAt = () => {
+  return <span className='italic font-medium'>Published On:{' '}</span>;
+}
+
+export { Source, Summary, PublishedAt }
