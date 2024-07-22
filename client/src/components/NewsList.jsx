a0a4fad78180092a889d30b823b6d10359c3f96f
praveenprk <praveenkumarkp666@gmail.com>
2024-07-23 01:25:34 +0530

    add basic spacing between each news listing

--- client/src/components/NewsList.jsx
+++ client/src/components/NewsList.jsx
@@ -3,7 +3,7 @@ import NewsItem from './NewsItem';
 
 const NewsList = ({ articles }) => {
   return (
-    <div>
+    <div className="space-y-4">
       {articles.map((article, index) => (
         <NewsItem key={index} article={article} />
       ))}
