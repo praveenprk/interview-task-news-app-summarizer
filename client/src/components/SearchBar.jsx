f58e2a1a29f03b610a71b1468c2d9a7b87e0318a
praveenprk <praveenkumarkp666@gmail.com>
2024-07-23 01:12:37 +0530

    add styling to search bar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -9,14 +9,15 @@ const SearchBar = ({ onSearch }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex justify-center mb-4">
       <input
+        className='border-solid border-2 border-sky-500 rounded-l-lg'
         type="text"
         value={keyword}
         onChange={(e) => setKeyword(e.target.value)}
         placeholder="Search for news articles..."
       />
-      <button type="submit">Search</button>
+      <button className="p-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-700" type="submit">Search</button>
     </form>
   );
 };
