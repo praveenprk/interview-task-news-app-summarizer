f8eb4540fbe6a6e81a7f8ccb718dbc5edc6cc24b
praveenprk <praveenkumarkp666@gmail.com>
2024-07-23 00:43:27 +0530

    adding loader component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,30 @@
-// src/App.js
 import React, { useState } from 'react';
 import axios from 'axios';
 import SearchBar from './components/SearchBar';
 import NewsList from './components/NewsList';
+import Loader from './components/Loader';
 
 const App = () => {
   const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+
 
   const fetchNews = async (keyword) => {
+    setLoading(true);
     try {
       const response = await axios.get(`http://localhost:5250/news?keyword=${keyword}`);
       setArticles(response.data.articles);
     } catch (error) {
       console.error("Error fetching news", error);
     }
+    setLoading(false);
   };
 
   return (
     <div>
       <h1>AI-Powered News Search Application</h1>
       <SearchBar onSearch={fetchNews} />
-      <NewsList articles={articles} />
+      {loading ? <Loader /> : <NewsList articles={articles} />}
     </div>
   );
 };
